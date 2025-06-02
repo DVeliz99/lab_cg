89e95d4b0a9f6ec36801d52498694030801999df
Dario Veliz <velizalexander99@gmail.com>
2025-06-01 19:27:32 -0600

    feat(user_data_source.dart,-user_repository_impl.dart,-user_repository.dart): agregar estructura inicial de Clean Arquitecture para User

---
+++ lib/data/data_sources/user_data_source.dart
@@ -0,0 +1,27 @@
+import 'package:lab_cg/domain/user.dart';
+
+abstract class UserDataSource {
+  Future<List<User>> getUsers();
+  Future<User?> getUserById(int id);
+  Future<void> createUser(User user);
+  Future<void> updateUser(User user);
+  Future<void> deleteUser(int id);
+}
+
+
+/*solicitudes a firebase */
+
+// class FirebaseUserDataSource implements UserDataSource{
+
+//   final FirebaseFirestore firestore;
+
+//   FirebaseUserDataSource({required this.firestore});
+
+//    @override
+//   Future<List<User>> getUsers() async {
+//     final snapshot = await firestore.collection('users').get();
+//     return snapshot.docs
+//         .map((doc) => User.fromJson(doc.data(), doc.id))
+//         .toList();
+//   }
+// }
